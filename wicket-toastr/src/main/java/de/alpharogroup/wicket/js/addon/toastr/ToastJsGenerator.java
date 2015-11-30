ae45b6c2e7a5c07766234b23178dab2bb6c8d6c7
astrapi69 <asterios.raptis@gmx.net>
2015-11-30 18:01:02 +0100

    version update after refactoring.

--- wicket-toastr/src/main/java/de/alpharogroup/wicket/js/addon/toastr/ToastJsGenerator.java
+++ wicket-toastr/src/main/java/de/alpharogroup/wicket/js/addon/toastr/ToastJsGenerator.java
@@ -19,7 +19,7 @@ import java.io.Serializable;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.util.lang.Args;
 
 import de.alpharogroup.wicket.js.addon.core.JavascriptGenerator;
